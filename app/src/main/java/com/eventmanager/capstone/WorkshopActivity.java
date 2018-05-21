fd236fb8e871cf028c0204eb4df67c3ed2e292ef
Alexandre Stelzig <Alexandre.Stelzig@gmail.com>
2018-05-21 19:58:50 +0900

    Add Database for Calendar Events

--- app/src/main/java/com/eventmanager/capstone/WorkshopActivity.java
+++ app/src/main/java/com/eventmanager/capstone/WorkshopActivity.java
@@ -5,10 +5,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.eventmanager.capstone.database.Database;
+import com.eventmanager.capstone.models.CalendarEventModel;
+
+import java.text.DateFormatSymbols;
 
 public class WorkshopActivity extends AppCompatActivity {
+
+
+    private int mCalendarEventId;
+
+    private TextView nameTextView;
+    private TextView timeTextView;
+    private TextView locationTextView;
+    private TextView descriptionTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +41,29 @@ public class WorkshopActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle(R.string.activty_workshop_title);
 
+        nameTextView = findViewById(R.id.workshop_name_text_view);
+        timeTextView = findViewById(R.id.workshop_time_text_view);
+        locationTextView = findViewById(R.id.workshop_location_text_view);
+        descriptionTextView = findViewById(R.id.workshop_description_text_view);
 
+
+        mCalendarEventId = getIntent().getIntExtra("EventId", -1);
+
+        if(mCalendarEventId == -1){
+            Toast.makeText(this, "Error while opening workshop", Toast.LENGTH_SHORT).show();
+            Intent intentCancel = new Intent();
+            setResult(Activity.RESULT_CANCELED, intentCancel);
+            finish();
+        }else{
+            CalendarEventModel calendarEvent =
+                    Database.mCalendarEventDao.fetchCalendarEventId(mCalendarEventId);
+
+            nameTextView.setText(calendarEvent.getName());
+            timeTextView.setText(calendarEvent.getStartTime() + " to " + calendarEvent.getEndTime() + ", "  + calendarEvent.getDayOfMonth() + " "
+                    + new DateFormatSymbols().getMonths()[calendarEvent.getMonth()-1] + " " + calendarEvent.getYear());
+            locationTextView.setText(calendarEvent.getLocation());
+            descriptionTextView.setText(calendarEvent.getDescription());
+        }
     }
 
     @Override
